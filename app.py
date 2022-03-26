8092b9c163851d62d6807e8f0b6465352d91f864
Victor Umunna <umunnavictor01@gmail.com>
2022-03-26 01:29:37 +0100

    update app

--- app.py
+++ app.py
@@ -5,49 +5,75 @@ from sklearn.preprocessing import LabelEncoder
 
 from lightgbm import LGBMRegressor
 
+
 def load_model():
-    with open('./models/regressor.pkl', 'rb') as file:
+    with open("./models/regressor.pkl", "rb") as file:
         data = pickle.load(file)
     return data
+
 
 data = load_model()
 
 model = data["model"]
-norm = data['normalization']
+norm = data["normalization"]
 
 le_manufacturer = LabelEncoder()
 le_engine = LabelEncoder()
 le_transmission = LabelEncoder()
 
+
 def main():
-    #build the main app
+    # build the main app
     st.title("USED CAR PRICE PREDICTION APP")
 
     st.write("""### We need some information to predict the price""")
 
     manufacturers = (
-        'Abarth', 'Alfa-Romero', 'Audi', 'BMW', 'Bentley', 'Chevrolet',
-       'Chrysler', 'Citroen', 'DS', 'Dacia', 'Fiat', 'Ford', 'Honda',
-       'Hyundai', 'Infiniti', 'Isuzu', 'Jaguar', 'Jeep', 'Kia',
-       'Land-Rover', 'Lexus', 'MG', 'Maserati', 'Mazda', 'Mercedes-Benz',
-       'Mini', 'Mitsubishi', 'Nissan', 'Peugeot', 'Porsche', 'Renault',
-       'Seat', 'Skoda', 'Smart', 'Subaru', 'Suzuki', 'Toyota', 'Vauxhall',
-       'Volkswagen', 'Volvo'
+        "Abarth",
+        "Alfa-Romero",
+        "Audi",
+        "BMW",
+        "Bentley",
+        "Chevrolet",
+        "Chrysler",
+        "Citroen",
+        "DS",
+        "Dacia",
+        "Fiat",
+        "Ford",
+        "Honda",
+        "Hyundai",
+        "Infiniti",
+        "Isuzu",
+        "Jaguar",
+        "Jeep",
+        "Kia",
+        "Land-Rover",
+        "Lexus",
+        "MG",
+        "Maserati",
+        "Mazda",
+        "Mercedes-Benz",
+        "Mini",
+        "Mitsubishi",
+        "Nissan",
+        "Peugeot",
+        "Porsche",
+        "Renault",
+        "Seat",
+        "Skoda",
+        "Smart",
+        "Subaru",
+        "Suzuki",
+        "Toyota",
+        "Vauxhall",
+        "Volkswagen",
+        "Volvo",
     )
 
-    engines = (
-        'Diesel', 
-        'Electric',
-        'Hybrid', 
-        'Petrol', 
-        'Plug_in_hybrid'
-    )
+    engines = ("Diesel", "Electric", "Hybrid", "Petrol", "Plug_in_hybrid")
 
-    transmissions = (
-        'Automatic',
-        'Manual',
-        'Semiautomatic'
-    )
+    transmissions = ("Automatic", "Manual", "Semiautomatic")
 
     manufacturer = st.selectbox("Manufacturer", manufacturers)
     age = st.slider("Age of Car", 1, 50, 1)
@@ -59,9 +85,9 @@ def main():
 
     if ok:
         X = np.array([[manufacturer, age, mileage, engine, transmission]])
-        X[:,0] = le_manufacturer.fit_transform(X[:,0])
-        X[:,3] = le_engine.fit_transform(X[:,3])
-        X[:,4] = le_transmission.fit_transform(X[:,4])
+        X[:, 0] = le_manufacturer.fit_transform(X[:, 0])
+        X[:, 3] = le_engine.fit_transform(X[:, 3])
+        X[:, 4] = le_transmission.fit_transform(X[:, 4])
         scaled_X = norm.transform(X)
 
         price = model.predict(scaled_X)
@@ -72,5 +98,5 @@ def main():
         st.subheader(f"The estimated cost of the car is {actual_price:,} pounds")
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     main()
